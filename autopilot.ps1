3e75589c7c288d10979cc1f9c5a717106b2a3270
GreGreek <gregory.grigoriadis@datasnipper.com>
2024-07-25 14:59:43 +0200

    Add files via upload

---
+++ autopilot.ps1
@@ -0,0 +1,20 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+New-Item -Type Directory -Path "C:\HWID"
+Set-Location -Path "C:\HWID"
+$env:Path += ";C:\Program Files\WindowsPowerShell\Scripts"
+
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned
+
+Install-Script -Name Get-WindowsAutopilotInfo
+
+# Get the serial number of the computer
+$serialNumber = (Get-WmiObject -Class Win32_BIOS).SerialNumber
+
+# Generate filename based on serial number or Intune device name
+if ($serialNumber -ne $null) {
+    $filename = "AutopilotHWID_$serialNumber.csv"
+} else {
+    $filename = "AutopilotHWID.csv"
+}
+
+Get-WindowsAutopilotInfo -OutputFile $filename
